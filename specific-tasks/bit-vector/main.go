b6c6e5e1a2fc2a724858e2570f7cc3e9fb071935
r.zhiharev <r.zhiharev@itlabrealty.ru>
2022-11-06 22:12:52 +0300

    feat(bit-vector): add string method

--- specific-tasks/bit-vector/main.go
+++ specific-tasks/bit-vector/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+)
 
 // целых чисел. Нулевое значение представляет пустое множество.
 type IntSet struct {
@@ -63,6 +66,27 @@ func (s *IntSet) UnionWith(t *IntSet) {
 			s.words = append(s.words, tword)
 		}
 	}
+}
+
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", 64*i+j)
+			}
+		}
+	}
+
+	buf.WriteByte('}')
+	return buf.String()
 }
 
 func main() {
